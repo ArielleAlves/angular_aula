c0e290bab5fae140d526d1f175a5c5883b62f6de
ArielleAlves <arielleandressaalves@gmail.com>
2019-02-28 22:44:55 -0300

    calculadora

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.css']
 })
 export class AppComponent{
+  nome: string = "Arielle";
+  valor: string = "asdfg";
 
+  val1: number = 18;
+  val2: number = 2;
+
+  somar(val1, val2) : number{
+    
+  }
+
+  subtrair(val1, val2) : number{
+
+  }
+
+  multiplicar(val1, val2) : number{
+
+  }
+
+  dividir(val1, val2) : number{
+
+  }
+
+clique() : void {
+  alert(this.nome);
+  }
 }
